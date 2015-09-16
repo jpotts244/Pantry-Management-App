dec1f858ac0ee92aa7d4f55c47960ccee158d939
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-09-16 11:31:56 -0400

    updates user and food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  validates :food_name, presence: true,
+  validates :quantity, presence: true,
+  validates :expiration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :foods
 	has_many :categories, through: :foods
+	validates :name, presence: true,
+	validates :email, presence: true, :uniqueness => {:case_sensitive => false}
 
 	has_secure_password
 end
