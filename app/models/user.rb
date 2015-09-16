c8f7041251a2e3a749428711d7fc91ff7c5fa020
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-09-16 12:11:38 -0400

    fixes syntax error in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :foods
 	has_many :categories, through: :foods
-	validates :name, presence: true,
-	validates :email, presence: true, :uniqueness => {:case_sensitive => false}
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: {case_sensitive: false}
+	validates :password, presence: true
 
 	has_secure_password
 end
