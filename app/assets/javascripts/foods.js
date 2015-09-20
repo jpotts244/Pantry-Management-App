6874a7525c73b171bbdca976f46e3bffb9c196a4
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-09-19 21:12:02 -0400

    moves js to foods.js

---
+++ app/assets/javascripts/foods.js
@@ -0,0 +1,33 @@
+$(document).on("click", ".delete-button", function(){
+	var element = $(this);
+	console.log(element.parent().data("id"));
+	var foodId = element.parent().data("id");
+
+	$.ajax({
+		url: "/users/<%= @user.id %>/foods/" + foodId,
+		type: "DELETE",
+		success: function(e){
+			element.parent().remove();
+		}
+	})
+});
+	
+var form = $("#new-food-form");
+form.submit(function(e){
+	$.ajax({
+		url: "<%= user_foods_path(@user) %>",
+		method: "POST",
+		data: form.serialize(),
+		success: function(data) {
+			console.log("success baby!");
+			$(".food-list").append(data);
+		}
+	});
+	e.preventDefault();
+});
+
+
+var editForm = $("#edit-food-form");
+$(document).on("click", ".edit-button", function(){
+	$(editForm).show();
+});
